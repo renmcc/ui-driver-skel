e10620063064a723107f42ae264852d19f99cdc0
renmcc <ren_mcc@foxmail.com>
2022-03-08 17:21:43 +0800

    first commit

---
+++ tmp/rexport.js
@@ -0,0 +1,15 @@
+"use strict";
+
+define("ui/components/driver-skel/component", ["exports", "nodes/components/driver-skel/component"], function (exports, _component) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  Object.defineProperty(exports, "default", {
+    enumerable: true,
+    get: function () {
+      return _component.default;
+    }
+  });
+});
